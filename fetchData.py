f4ddbf4571162a69cfd74a010c8946cc3942d0ba
sanamusharraf <43441901+sanamusharraf@users.noreply.github.com>
2019-10-09 16:30:55 +0500

    Add files via upload

---
+++ fetchData.py
@@ -0,0 +1,28 @@
+import mysql.connector
+import os
+from PIL import Image
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="test"
+)
+my_database=mydb.cursor()
+sql_statement = "SELECT programID,thumbimage FROM `log_programbm` group by programID order by insertDate"
+my_database.execute(sql_statement)
+output = my_database.fetchall()
+
+Thisfolder = os.path.dirname(os.path.abspath(__file__))
+folder = os.path.join(Thisfolder , "internship/pictures")
+for x in output:
+  name = str(x[0])
+  picture = os.path.join(folder, "{names}.png".format(names=name))
+  
+  with open(picture,'wb') as file:
+    file.write(x[1])
+    file.close()
+
+  img = Image.open(picture)  
+  img = img.resize((50, 50)) 
+  img.save(picture) 
